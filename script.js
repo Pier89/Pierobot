9f9da545ca53cc753f8ed0941de3224b81b304ee
Pier89 <kurdt.piero@gmail.com>
2016-05-31 10:58:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Bot bot bot! Do you want to know more about Pierfrancesco? Just say HELLO and let's start!!!.')
                 .then(() => 'speak');
         }
     },
